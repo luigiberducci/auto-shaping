953ad426b42f82bdac106b2826338eeb3967cc09
Luigi Berducci <luigi.berd@gmail.com>
2023-11-20 13:45:50 +0100

    refactoring run benchmark logging and vis

--- run_benchmark.py
+++ run_benchmark.py
@@ -1,5 +1,7 @@
 import os
 import logging
+import pathlib
+import re
 from argparse import Namespace
 
 import numpy as np
@@ -27,23 +29,41 @@ config = {
 }
 
 # run benchmark
-for reward in ["default", "HPRS", "TLTL", "BHNR"]:
+algo, env_id = config["algo"], config["env_id"]
+
+rewards = ["default", "HPRS", "TLTL", "BHNR"]
+for reward in rewards:
+    break
     print(f"Training with {reward} reward shaping")
 
     cfg = config.copy()
     cfg["train_reward"] = reward
+    cfg["wandb_group"] = f"{algo}-{env_id}-{reward}"
     run.main(args=Namespace(**cfg))
 
 # load results from logdirectory
-# todo
 results = {}
+for reward in rewards:
+    eval_files = list(pathlib.Path(config["log_dir"]).glob(f"**/{algo}-{env_id}-{reward}/*/evaluations.npz"))
+
+    if len(eval_files) > 1:
+        logging.warning(f"Found {len(eval_files)} eval files for {algo}-{env_id}-{reward}. Plotting only the 1st one.")
+
+    file = eval_files[0]
+    if len(re.findall(f"{algo}-{env_id}-{reward}", str(file.parent))) > 0:
+        data = np.load(file, allow_pickle=True)
+        results[reward] = {
+            "timesteps": data["timesteps"],
+            "rewards": data["results"],
+            "lengths": data["ep_lengths"],
+        }
 
 # plot learning curves
 if len(results) > 0:
     import matplotlib.pyplot as plt
 
     plt.figure(figsize=(10, 5))
-    plt.title(f"Learning Curves - {cfg['env_id']} - {cfg['algo']}")
+    plt.title(f"Learning Curves - {env_id} - {algo}")
 
     for reward in results:
         rewards = results[reward]["rewards"]
